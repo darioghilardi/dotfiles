3c643048a00f4e2b6c45caab85c5544562125dfe
Dario Ghilardi <darioghilardi@webrain.it>
2022-05-02 23:20:34 +0200

    feat(git): enable gh

--- home/git.nix
+++ home/git.nix
@@ -1,16 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  programs.git = {
-    enable = true;
-    userName = "Dario Ghilardi";
-    userEmail = "darioghilardi@webrain.it";
-    ignores = [ ".DS_Store" ];
-    extraConfig = {
-      color.ui = "auto";
-      init = { defaultBranch = "master"; };
-    };
+  programs.git.enable = true;
+
+  programs.git.userName = "Dario Ghilardi";
+  programs.git.userEmail = "darioghilardi@webrain.it";
+  programs.git.ignores = [ ".DS_Store" ];
+  programs.git.extraConfig = {
+    color.ui = "auto";
+    init = { defaultBranch = "master"; };
   };
 
+  # Enhanced diffs
   programs.git.delta.enable = true;
+
+  # GitHub CLI
+  programs.gh.enable = true;
+  programs.gh.settings = {
+    git_protocol = "https";
+    prompt = "enabled";
+  };
 }
