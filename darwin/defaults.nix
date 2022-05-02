251cac0648aa219f9261ae89220f4696bb97945c
Dario Ghilardi <darioghilardi@webrain.it>
2022-05-02 13:09:53 +0200

    feat(dock): do not show recent apps

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -2,16 +2,16 @@
   system.defaults.NSGlobalDomain = {
     # scroll direction
     "com.apple.swipescrolldirection" = false;
-    # Keyboard key repeat
+    # keyboard key repeat
     InitialKeyRepeat = 15;
     KeyRepeat = 2;
-    
   };
 
   # Dock and Mission Control
   system.defaults.dock = {
     autohide = false;
     tilesize = 32;
+    show-recents = false;
   };
 
   system.defaults.trackpad = {
