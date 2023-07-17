ae40c35fa50d8b88996737e09697b83ebae17a2b
Dario Ghilardi <darioghilardi@webrain.it>
2023-07-17 11:13:39 +0200

    fix: disable shell integration and get back the block cursor

--- home/kitty.nix
+++ home/kitty.nix
@@ -33,6 +33,7 @@ in {
     font_size = "14.0";
     adjust_line_height = "140%";
     disable_ligatures = "cursor"; # disable ligatures when cursor is on them
+    shell_integration = "no-cursor";
 
     # Window layout
     window_padding_width = "10";
