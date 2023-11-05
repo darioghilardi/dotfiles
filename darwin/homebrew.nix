c9740c9d661fc961a93450846175ed81e6c77489
Dario Ghilardi <darioghilardi@webrain.it>
2023-11-05 23:38:00 +0100

    feat: force bump all casks and bump flake

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -5,65 +5,12 @@
   ...
 }: let
   inherit (lib) mkIf;
+
   mkIfCaskPresent = cask: mkIf (lib.any (x: x == cask) config.homebrew.casks);
+
   brewEnabled = config.homebrew.enable;
-in {
-  environment.shellInit = mkIf brewEnabled ''
-    eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
-  '';
 
-  # https://docs.brew.sh/Shell-Completion#configuring-completions-in-fish
-  # For some reason if the Fish completions are added at the end of `fish_complete_path` they don't
-  # seem to work, but they do work if added at the start.
-  programs.fish.interactiveShellInit = mkIf brewEnabled ''
-    if test -d (brew --prefix)"/share/fish/completions"
-      set -p fish_complete_path (brew --prefix)/share/fish/completions
-    end
-    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-      set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-    end
-  '';
-
-  homebrew.enable = true;
-  homebrew.onActivation.upgrade = true;
-  homebrew.onActivation.autoUpdate = true;
-  homebrew.onActivation.cleanup = "zap";
-  homebrew.global.brewfile = true;
-  homebrew.global.lockfiles = true;
-
-  homebrew.taps = [
-    "homebrew/cask"
-    "homebrew/cask-drivers"
-    "homebrew/cask-fonts"
-    "homebrew/cask-versions"
-    "homebrew/core"
-    "homebrew/services"
-    "nrlquaker/createzap"
-  ];
-
-  # Prefer installing application from the Mac App Store
-  #
-  # Commented apps suffer continual update issue:
-  # https://github.com/malob/nixpkgs/issues/9
-  homebrew.masApps = {
-    "GIPHY Capture. The GIF Maker" = 668208984;
-    Keynote = 409183694;
-    Numbers = 409203825;
-    Pages = 409201541;
-    Slack = 803453959;
-    "Spark - App email di Readdle" = 1176895641;
-    Tailscale = 1475387142;
-    "Things 3" = 904280696;
-    Twitter = 1482454543;
-    "1password for Safari" = 1569813296;
-    rcmd = 1596283165;
-    amphetamine = 937984704;
-    wireguard = 1451685025;
-  };
-
-  # If an app isn't available in the Mac App Store, or the version in the App Store has
-  # limitiations, e.g., Transmit, install the Homebrew Cask.
-  homebrew.casks = [
+  casks = [
     "1password-beta"
     "1password-cli"
     "airflow"
@@ -105,14 +52,71 @@ in {
     "vlc"
     "vmware-fusion"
     "zoom"
-
-    # Those other apps comes from setapp:
-    # "bettertouchtool"
-    # "istat-menus"
-    # "tableplus"
-    # "sip"
-    # "pixelsnap"
   ];
+
+  # Compute the casks likt to set the greedy attribute on all casks.
+  greedyCasks =
+    builtins.map (c: {
+      name = c;
+      greedy = true;
+    })
+    casks;
+in {
+  environment.shellInit = mkIf brewEnabled ''
+    eval "$(${config.homebrew.brewPrefix}/brew shellenv)"
+  '';
+
+  # https://docs.brew.sh/Shell-Completion#configuring-completions-in-fish
+  # For some reason if the Fish completions are added at the end of `fish_complete_path` they don't
+  # seem to work, but they do work if added at the start.
+  programs.fish.interactiveShellInit = mkIf brewEnabled ''
+    if test -d (brew --prefix)"/share/fish/completions"
+      set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+      set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+  '';
+
+  homebrew.enable = true;
+  homebrew.global.brewfile = true;
+  homebrew.onActivation = {
+    upgrade = true;
+    autoUpdate = true;
+    cleanup = "zap";
+  };
+
+  homebrew.taps = [
+    "homebrew/cask"
+    "homebrew/cask-fonts"
+    "homebrew/cask-versions"
+    "homebrew/services"
+    "nrlquaker/createzap"
+  ];
+
+  # Prefer installing application from the Mac App Store
+  #
+  # Commented apps suffer continual update issue:
+  # https://github.com/malob/nixpkgs/issues/9
+  homebrew.masApps = {
+    "GIPHY Capture. The GIF Maker" = 668208984;
+    Keynote = 409183694;
+    Numbers = 409203825;
+    Pages = 409201541;
+    Slack = 803453959;
+    "Spark - App email di Readdle" = 1176895641;
+    Tailscale = 1475387142;
+    "Things 3" = 904280696;
+    Twitter = 1482454543;
+    "1password for Safari" = 1569813296;
+    rcmd = 1596283165;
+    amphetamine = 937984704;
+    wireguard = 1451685025;
+  };
+
+  # If an app isn't available in the Mac App Store, or the version in the App Store has
+  # limitiations, e.g., Transmit, install the Homebrew Cask.
+  homebrew.casks = greedyCasks;
 
   # Until these are managed by nix.
   # homebrew.brews = ["tfenv"]; # [ "tfenv" "asdf" "unixodbc" "wxwidgets" ];
