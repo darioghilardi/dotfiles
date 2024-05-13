ad3c5f749034f165a7f421de7392fad0b3bb1872
Dario Ghilardi <darioghilardi@webrain.it>
2024-05-13 12:23:23 +0200

    feat(zed): basic config for zed

--- modules/home/apps/zed/default.nix
+++ modules/home/apps/zed/default.nix
@@ -19,8 +19,46 @@ in {
       {
         "ui_font_size": 16,
         "buffer_font_size": 14,
-        "vim_mode": true
+        "tab_size": 2,
+        "vim_mode": true,
+        "toolbar": {
+          "breadcrumbs": false,
+          "quick_actions": false
+        },
+        "git": {
+          "inline_blame": {
+            "enabled": false
+          }
+        }
       }
+    '';
+
+    home.file.".config/zed/keymap.json".text = ''
+      [
+        {
+          "context": "Editor && vim_mode == normal && (vim_operator == none || vim_operator == n) && !VimWaiting",
+          "bindings": {
+            "space f s": "workspace::Save",
+            "space p t": "workspace::ToggleLeftDock",
+            "space p f": "file_finder::Toggle",
+            "space s p": "project_panel::NewSearchInDirectory",
+            "space w v": "pane::SplitLeft",
+            "space w s": "pane::SplitUp",
+            "space w d": ["pane::CloseActiveItem", { "saveIntent": "skip" }],
+            "space w h": ["workspace::ActivatePaneInDirection", "Left"],
+            "space w l": ["workspace::ActivatePaneInDirection", "Right"],
+            "space w k": ["workspace::ActivatePaneInDirection", "Up"],
+            "space w j": ["workspace::ActivatePaneInDirection", "Down"],
+            "space g b": "editor::ToggleGitBlame"
+          }
+        },
+        {
+          "context": "Editor && vim_mode == insert && (vim_operator == none || vim_operator == n) && !VimWaiting",
+          "bindings": {
+            "f d": ["vim::SwitchMode", "Normal"]
+          }
+        }
+      ]
     '';
   };
 }
