3125bfd029f0be7e4ee24c47be736750ca68c28a
Dario Ghilardi <darioghilardi@webrain.it>
2024-08-22 14:59:01 +0200

    feat: hide tab bar in zed

--- modules/home/apps/zed/default.nix
+++ modules/home/apps/zed/default.nix
@@ -20,6 +20,9 @@ in {
         "ui_font_size": 16,
         "buffer_font_size": 14,
         "tab_size": 2,
+        "tab_bar": {
+          "show": false
+        },
         "vim_mode": true,
         "toolbar": {
           "breadcrumbs": false,
