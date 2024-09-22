8eaebb6375332a132dfe5134fd3f4f6db653c301
Dario Ghilardi <darioghilardi@webrain.it>
2024-09-22 23:42:47 +0200

    feat(network-unlock): try to unlock via ssh

--- scripts/testvm/hardware-configuration.tpl.nix
+++ scripts/testvm/hardware-configuration.tpl.nix
@@ -12,8 +12,6 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "sr_mod"];
-  boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
 
