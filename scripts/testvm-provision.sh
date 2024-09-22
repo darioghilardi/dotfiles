e75d22eb7daca304898b98b349d0e75666a895a4
Dario Ghilardi <darioghilardi@webrain.it>
2024-09-22 11:25:25 +0200

    provision changes

--- scripts/testvm-provision.sh
+++ scripts/testvm-provision.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ssh-copy-id nixos@192.168.65.2
+ssh-copy-id -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" nixos@192.168.65.2
 scp -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" scripts/testvm-install.sh nixos@192.168.65.2:/home/nixos/installer.sh
 ssh -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" nixos@192.168.65.2 'chmod +x ~/installer.sh'
+ssh -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" nixos@192.168.65.2 'sudo ssh-keygen -t ed25519 -N "" -f /home/ssh_host_ed25519_key'
 ssh -q -o StrictHostKeyChecking=no -o "UserKnownHostsFile /dev/null" nixos@192.168.65.2 'sudo ~/installer.sh'
