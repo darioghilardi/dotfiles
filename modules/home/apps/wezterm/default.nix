e4c04de92c00a2815257435733c1b8bf65672205
Dario Ghilardi <darioghilardi@webrain.it>
2024-12-31 13:35:55 +0100

    fix(wezterm): missing comma

--- modules/home/apps/wezterm/default.nix
+++ modules/home/apps/wezterm/default.nix
@@ -31,7 +31,7 @@ in {
           font_size = 14.0,
           freetype_load_target = 'Light',
           freetype_render_target = 'HorizontalLcd',
-          color_scheme = 'Solarized Dark - Patched'
+          color_scheme = 'Solarized Dark - Patched',
           cell_width = 0.9,
           hide_tab_bar_if_only_one_tab = true,
           use_fancy_tab_bar = true,
