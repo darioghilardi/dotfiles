3d5f9fcfa0365f0a61f9db5f382d5d5da5d82629
Dario Ghilardi <darioghilardi@webrain.it>
2025-07-29 09:38:00 +0200

    bump flake, comment zed config

--- modules/home/apps/zed/default.nix
+++ modules/home/apps/zed/default.nix
@@ -5,9 +5,11 @@
   ...
 }:
 with lib;
-with lib.dariodots; let
+with lib.dariodots;
+let
   cfg = config.dariodots.apps.zed;
-in {
+in
+{
   # Zed configuration (zed installed through brew for now)
 
   options.dariodots.apps.zed = with types; {
@@ -15,53 +17,58 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.file.".config/zed/settings.json".text = ''
-      {
-        "ui_font_size": 16,
-        "buffer_font_size": 14,
-        "tab_size": 2,
-        "tab_bar": {
-          "show": false
-        },
-        "vim_mode": true,
-        "toolbar": {
-          "breadcrumbs": false,
-          "quick_actions": false
-        },
-        "git": {
-          "inline_blame": {
-            "enabled": false
-          }
-        }
-      }
-    '';
+    # Still experimenting, config to be moved here when it's stable enough.
+    # home.file.".config/zed/settings.json".text = ''
+    #   {
+    #     "ui_font_size": 16,
+    #     "buffer_font_size": 13,
+    #     "tab_size": 2,
+    #     "tab_bar": {
+    #       "show": false
+    #     },
+    #     "toolbar": {
+    #       "breadcrumbs": false,
+    #       "quick_actions": false
+    #     },
+    #     "git": {
+    #       "inline_blame": {
+    #         "enabled": false
+    #       }
+    #     },
+    #     "theme": {
+    #       "mode": "dark",
+    #       "dark": "One Dark"
+    #     },
+    #     "vim_mode": true
+    #   }
+    # '';
 
-    home.file.".config/zed/keymap.json".text = ''
-      [
-        {
-          "context": "Editor && vim_mode == normal && (vim_operator == none || vim_operator == n) && !VimWaiting",
-          "bindings": {
-            "space f s": "workspace::Save",
-            "space p t": "workspace::ToggleLeftDock",
-            "space p f": "file_finder::Toggle",
-            "space s p": "project_panel::NewSearchInDirectory",
-            "space w v": "pane::SplitLeft",
-            "space w s": "pane::SplitUp",
-            "space w d": ["pane::CloseAllItems", { "close_pinned": false }],
-            "space w h": ["workspace::ActivatePaneInDirection", "Left"],
-            "space w l": ["workspace::ActivatePaneInDirection", "Right"],
-            "space w k": ["workspace::ActivatePaneInDirection", "Up"],
-            "space w j": ["workspace::ActivatePaneInDirection", "Down"],
-            "space g b": "editor::ToggleGitBlame"
-          }
-        },
-        {
-          "context": "Editor && vim_mode == insert && (vim_operator == none || vim_operator == n) && !VimWaiting",
-          "bindings": {
-            "f d": ["vim::SwitchMode", "Normal"]
-          }
-        }
-      ]
-    '';
+    # home.file.".config/zed/keymap.json".text = ''
+    #   [
+    #     {
+    #       "context": "Editor && vim_mode == normal && (vim_operator == none || vim_operator == n) && !VimWaiting",
+    #       "bindings": {
+    #         "space f s": "workspace::Save",
+    #         "space p t": "workspace::ToggleLeftDock",
+    #         "space p f": "file_finder::Toggle",
+    #         "space w v": "pane::SplitLeft",
+    #         "space w s": "pane::SplitUp",
+    #         "space w d": ["pane::CloseAllItems", { "close_pinned": false }],
+    #         "space w h": ["workspace::ActivatePaneInDirection", "Left"],
+    #         "space w l": ["workspace::ActivatePaneInDirection", "Right"],
+    #         "space w k": ["workspace::ActivatePaneInDirection", "Up"],
+    #         "space w j": ["workspace::ActivatePaneInDirection", "Down"],
+    #         "space g b": "editor::ToggleGitBlame",
+    #         "space t n": "workspace::ToggleBottomDock"
+    #       }
+    #     },
+    #     {
+    #       "context": "Editor && vim_mode == insert && (vim_operator == none || vim_operator == n) && !VimWaiting",
+    #       "bindings": {
+    #         "f d": ["vim::SwitchMode", "Normal"]
+    #       }
+    #     }
+    #   ]
+    # '';
   };
 }
