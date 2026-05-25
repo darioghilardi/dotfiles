6820dc634c1ac387ed65b783a0e629d3f5aa9ed9
Dario Ghilardi <darioghilardi@webrain.it>
2026-05-25 14:17:43 +0200

    fix(nix): nix is managed by determinate nix

--- modules/darwin/nix/default.nix
+++ modules/darwin/nix/default.nix
@@ -16,11 +16,6 @@
   config,
   ...
 }: {
-  nix.enable = true;
-
-  nix.settings = {
-    substituters = ["https://cache.nixos.org/"];
-    trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
-    trusted-users = ["@admin"];
-  };
+  # DeterminateSystem installer manages the nix daemon.
+  nix.enable = false;
 }
